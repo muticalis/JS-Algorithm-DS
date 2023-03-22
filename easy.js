5676726973dc48bdccc60dfed90045e6db7856fa
Mustafa Çalış <muticalis@gmail.com>
2023-03-22 21:30:30 +0300

    last commit

--- easy.js
+++ easy.js
@@ -1,3 +1,8 @@
-import { userValidation } from "./UserValidation/uservalidation.js";
+//import { userValidation } from "./UserValidation/uservalidation.js";
+//console.log(userValidation("u__hello_world123"))
 
-console.log(userValidation("u__hello_world123"))
+
+
+
+import { longestWord } from "./LongestWord/longestword.js";
+console.log(longestWord("fun&!! time"))
