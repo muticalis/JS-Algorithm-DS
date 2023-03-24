9395cab4ddce9bcdfcbe88cb841f4f6eccae9ff4
Mustafa Çalış <muticalis@gmail.com>
2023-03-24 20:25:39 +0300

    FirstReverse solition

---
+++ FirstReverse/reverse.js
@@ -0,0 +1,12 @@
+export const firstReverse = (str) => {
+
+
+
+    return str.split("").reverse().join("")
+}
+
+
+
+/*
+Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+*/
--- easy.js
+++ easy.js
@@ -2,10 +2,11 @@
 //console.log(userValidation("u__hello_world123"))
 
 
-
 //import { longestWord } from "./LongestWord/longestworld.js";
 //console.log(longestWord("fun&!! time"))
 
-import { firstFactorial } from "./FirstFactorial/firstfactorial.js";
+//import { firstFactorial } from "./FirstFactorial/firstfactorial.js";
+//console.log(firstFactorial(8))
 
-console.log(firstFactorial(8))
+import { firstReverse } from "./FirstReverse/reverse.js";
+console.log(firstReverse("merhaba"))
